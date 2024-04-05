620c0d11e63ed50c87104929e4b2653fe3ec4d1c
Alok <alokkhalkho65@gmail.com>
2024-04-05 20:47:55 +0530

    support page heading changed

--- src/components/SupportPage.jsx
+++ src/components/SupportPage.jsx
@@ -6,7 +6,7 @@ const SupportPage = () => {
         <div className="bg-white py-20 px-8 md:px-16 lg:px-32">
             <div className="flex flex-col md:flex-row items-center justify-between gap-20  ">
                 <div className='flex flex-col gap-4 md:w-1/2'>
-                    <h1 className="text-5xl font-semibold text-center md:text-left">Explore our APIs & SDKs</h1>
+                    <h1 className="text-5xl font-semibold text-center md:text-left">Looking for something else?</h1>
                     <p className="mb-8 text-center md:text-left">
                         If you need additional help, check out our support articles, community resources, and more.
                     </p>
@@ -14,7 +14,7 @@ const SupportPage = () => {
                         Find support
                     </button>
                 </div>
-                <div className="hidden md:block md:w-1/2">
+                <div className="hidden md:block md:size-80">
                     <img src={Confused} alt="Confused" className="size-96 ml-10" />
                 </div>
             </div>
